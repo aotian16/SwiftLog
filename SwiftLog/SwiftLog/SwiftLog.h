4aff86e7795649c741d70e462beed32b9f61e8c5
tongjin <tongjin@51cailai.com>
2015-10-26 17:31:52 +0800

    add SwiftLog

---
+++ SwiftLog/SwiftLog/SwiftLog.h
@@ -0,0 +1,19 @@
+//
+//  SwiftLog.h
+//  SwiftLog
+//
+//  Created by 童进 on 15/10/26.
+//  Copyright © 2015年 qefee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftLog.
+FOUNDATION_EXPORT double SwiftLogVersionNumber;
+
+//! Project version string for SwiftLog.
+FOUNDATION_EXPORT const unsigned char SwiftLogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftLog/PublicHeader.h>
+
+
---
+++ SwiftLog/SwiftLog/SwiftLog.swift
@@ -0,0 +1,141 @@
+//
+//  SwiftLog.swift
+//  SwiftLogDemo
+//
+//  Created by 童进 on 15/10/12.
+//  Copyright © 2015年 qefee. All rights reserved.
+//
+
+import Foundation
+
+public class SwiftLog {
+    /// log level. Print logs which >= logLevel
+    public var logLevel: LogLevel
+    /// date format
+    public var dateFormat: String = "yyyy-MM-dd HH:mm:ss.SSS"
+    {
+        didSet {
+            dateFormatter.dateFormat = dateFormat
+        }
+    }
+    /// date formatter
+    private var dateFormatter: NSDateFormatter = NSDateFormatter()
+
+    /**
+    init with LogLevel.Debug
+    */
+    public convenience init() {
+        self.init(logLevel: LogLevel.Debug)
+    }
+
+    public init(logLevel: LogLevel) {
+        self.logLevel = logLevel
+        self.dateFormatter.dateFormat = dateFormat
+    }
+    
+    /**
+    print verbose log.
+    
+    - parameter msg:          msg
+    - parameter fileName:     fileName
+    - parameter functionName: functionName
+    - parameter lineNum:      lineNum
+    - parameter columnNum:    columnNum
+    */
+    public func v(msg: String, fileName: String = __FILE__, functionName: String = __FUNCTION__, lineNum: Int = __LINE__, columnNum: Int = __COLUMN__) {
+        printLog(LogLevel.Verbose, msg: msg, fileName: fileName, functionName: functionName, lineNum: lineNum, columnNum: columnNum)
+    }
+    
+    /**
+    print debug log.
+    
+    - parameter msg:          msg
+    - parameter fileName:     fileName
+    - parameter functionName: functionName
+    - parameter lineNum:      lineNum
+    - parameter columnNum:    columnNum
+    */
+    public func d(msg: String, fileName: String = __FILE__, functionName: String = __FUNCTION__, lineNum: Int = __LINE__, columnNum: Int = __COLUMN__) {
+        printLog(LogLevel.Debug, msg: msg, fileName: fileName, functionName: functionName, lineNum: lineNum, columnNum: columnNum)
+    }
+    
+    /**
+    print info log.
+    
+    - parameter msg:          msg
+    - parameter fileName:     fileName
+    - parameter functionName: functionName
+    - parameter lineNum:      lineNum
+    - parameter columnNum:    columnNum
+    */
+    public func i(msg: String, fileName: String = __FILE__, functionName: String = __FUNCTION__, lineNum: Int = __LINE__, columnNum: Int = __COLUMN__) {
+        printLog(LogLevel.Info, msg: msg, fileName: fileName, functionName: functionName, lineNum: lineNum, columnNum: columnNum)
+    }
+    
+    /**
+    print warn log.
+    
+    - parameter msg:          msg
+    - parameter fileName:     fileName
+    - parameter functionName: functionName
+    - parameter lineNum:      lineNum
+    - parameter columnNum:    columnNum
+    */
+    public func w(msg: String, fileName: String = __FILE__, functionName: String = __FUNCTION__, lineNum: Int = __LINE__, columnNum: Int = __COLUMN__) {
+        printLog(LogLevel.Warn, msg: msg, fileName: fileName, functionName: functionName, lineNum: lineNum, columnNum: columnNum)
+    }
+    
+    /**
+    print error log.
+    
+    - parameter msg:          msg
+    - parameter fileName:     fileName
+    - parameter functionName: functionName
+    - parameter lineNum:      lineNum
+    - parameter columnNum:    columnNum
+    */
+    public func e(msg: String, fileName: String = __FILE__, functionName: String = __FUNCTION__, lineNum: Int = __LINE__, columnNum: Int = __COLUMN__) {
+        printLog(LogLevel.Error, msg: msg, fileName: fileName, functionName: functionName, lineNum: lineNum, columnNum: columnNum)
+    }
+    
+    /**
+    print assert log.
+    
+    - parameter msg:          msg
+    - parameter fileName:     fileName
+    - parameter functionName: functionName
+    - parameter lineNum:      lineNum
+    - parameter columnNum:    columnNum
+    */
+    public func a(msg: String, fileName: String = __FILE__, functionName: String = __FUNCTION__, lineNum: Int = __LINE__, columnNum: Int = __COLUMN__) {
+        printLog(LogLevel.Assert, msg: msg, fileName: fileName, functionName: functionName, lineNum: lineNum, columnNum: columnNum)
+    }
+    
+    /**
+    print log.
+    If you want to custom log format, change this function.
+    
+    - parameter logLevel:     logLevel
+    - parameter msg:          msg
+    - parameter fileName:     fileName
+    - parameter functionName: functionName
+    - parameter lineNum:      lineNum
+    - parameter columnNum:    columnNum
+    */
+    public func printLog(logLevel: LogLevel, msg: String, fileName: String = __FILE__, functionName: String = __FUNCTION__, lineNum: Int = __LINE__, columnNum: Int = __COLUMN__) {
+        if self.logLevel > logLevel {
+            return
+        }
+        
+        let date = dateFormatter.stringFromDate(NSDate())
+        let file = NSString(string: fileName).lastPathComponent
+        let fun = functionName
+        let line = lineNum
+        let col = columnNum
+        let lvl = logLevel
+        
+        print("\(date) \(file) : \(fun) : \(line) : \(col) \(lvl) \(msg)")
+    }
+}
+
+
